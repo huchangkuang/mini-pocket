c44f950fe6aef78a55c13751cda156a1be21ab82
huchangying <603021137@qq.com>
2023-02-27 11:02:34 +0800

    随机数优化

--- client/src/utils/generateNum.ts
+++ client/src/utils/generateNum.ts
@@ -1,10 +1,10 @@
-export const randomNum = (num: number) => Math.round(Math.random() * num)
+export const randomNum = (start: number, end: number) => (Math.ceil(Math.random() * end) - start + 1)
 
 export const generateNumList = (total = 33, resLength = 6) => {
   const redBallPool = new Array(total).fill("").map((_, index) => index + 1)
   const redResult:number[] = []
   for (let i = 0; i < resLength; i++) {
-    const index = randomNum(total - i - 1)
+    const index = randomNum(0, total - i - 1)
     const [item] = redBallPool.splice(index, 1)
     redResult.push(item)
   }
