211b140645b058303c1bdb08c9660948cf1ce77f
huchangying <603021137@qq.com>
2023-07-14 16:54:42 +0800

    弹幕页面优化

--- src/pages/handsBarrage/index.tsx
+++ src/pages/handsBarrage/index.tsx
@@ -1,22 +1,41 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useRef } from "react";
 import { View } from "@tarojs/components";
 import "./index.less";
-import Taro, { getSystemInfoSync, useRouter } from "@tarojs/taro";
+import Taro from "@tarojs/taro";
 import classNames from "classnames";
+import { useDebounce } from "@/hooks/useDebounce";
 
 const HandsBarrage: React.FC = () => {
   const {
     params: { data = "" },
-  } = useRouter();
-  const { windowWidth, windowHeight } = getSystemInfoSync();
+  } = Taro.useRouter();
+  const { windowWidth, windowHeight } = Taro.getSystemInfoSync();
+  const confirmQuit = useRef(false);
   const { fontSize, fontColor, time, barrage, bgColor } = useMemo(() => {
     if (data) {
       return JSON.parse(decodeURIComponent(data));
     }
     return {};
   }, [data]);
+  const exist = () => {
+    if (!confirmQuit.current) {
+      Taro.showToast({
+        title: "再次点击可退出弹幕",
+        icon: "none",
+        duration: 500,
+      });
+      confirmQuit.current = true;
+      onQuit();
+      return;
+    }
+    Taro.navigateBack();
+  };
+  const onQuit = useDebounce(() => {
+    confirmQuit.current = false;
+  }, 500);
   return (
     <View
+      onClick={exist}
       className={classNames(
         "handsBarrage",
         windowWidth > windowHeight && "noRotate"
