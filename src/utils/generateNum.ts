2c136b2a076f9ff7ce2c96f56b06a6a283cc7238
huchangying <603021137@qq.com>
2023-08-22 11:35:34 +0800

    fix: 添加测试用例

--- src/utils/generateNum.ts
+++ src/utils/generateNum.ts
@@ -1,5 +1,5 @@
 export const randomNum = (start: number, total: number) =>
-  Math.ceil(Math.random() * total || 1) + start - 1;
+  Math.floor(Math.random() * total) + start;
 
 export const generateNumList = (total = 33, resLength = 6) => {
   const redBallPool = new Array(total).fill("").map((_, index) => index + 1);
